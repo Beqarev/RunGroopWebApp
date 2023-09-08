45aba512f27a93681c4fc988bfd25def18f32029
Beqa <b.revazishvili2004@gmail.com>
2023-09-08 16:12:18 +0400

    fixed photo upload and aslo added delte pages

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Identity;
 
 namespace RunGroopWebApp.Models;
 
-public class AppUser
+public class AppUser : IdentityUser
 {
     [Key]
     public string Id { get; set; }
     public int? Pace { get; set; }
     public int? Mileage { get; set; }
+    public int AddressId { get; set; }
     public Address? Address { get; set; }
     public ICollection<Club> Clubs { get; set; }
     public ICollection<Race> Races { get; set; }
